c791050db85228edcbf7c5cdd792dd4980a749e3
Afrizzal P. Pratama <apratama8@gmail.com>
2021-09-05 01:11:06 +0700

    add userHandler

--- handler/user.go
+++ handler/user.go
@@ -1,1 +1,32 @@
 package handler
+
+import (
+	"net/http"
+	"startcare/user"
+
+	"github.com/gin-gonic/gin"
+)
+
+type userHandler struct {
+	userService user.Service
+}
+
+func newUserHandler(userService user.Service) *userHandler {
+	return &userHandler{userService}
+}
+
+func (h *userHandler) RegisterUser(c *gin.Context) {
+	//
+	var input user.RegisterUserInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+	user, err := h.userService.RegisterUser(input)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, nil)
+	}
+
+	c.JSON(http.StatusOK, user)
+}

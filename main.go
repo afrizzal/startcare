a03231cd36270165a5c973021432ccb630f8ecdd
Afrizzal P. Pratama <apratama8@gmail.com>
2021-09-26 02:33:12 +0700

    authMiddleware

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"fmt"
 	"log"
+	"net/http"
 	"startcare/auth"
 	"startcare/handler"
+	"startcare/helper"
 	"startcare/user"
+	"strings"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -48,16 +51,16 @@ func main() {
 	userService := user.NewService(userRepository)
 	authService := auth.NewService()
 
-	token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo0fQ.5uLQPYy4C1VKJ9cr05xswjgH0gwk8uTSiGltkAdHhVM")
-	if err != nil {
-		fmt.Println("ERROR")
-	}
+	// token, err := authService.ValidateToken("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjo0fQ.5uLQPYy4C1VKJ9cr05xswjgH0gwk8uTSiGltkAdHhVM")
+	// if err != nil {
+	// 	fmt.Println("ERROR")
+	// }
 
-	if token.Valid {
-		fmt.Println("VALID")
-	} else {
-		fmt.Println("INVALID")
-	}
+	// if token.Valid {
+	// 	fmt.Println("VALID")
+	// } else {
+	// 	fmt.Println("INVALID")
+	// }
 
 	// fmt.Println(authService.GenerateToken(1001))
 
@@ -71,13 +74,50 @@ func main() {
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
 	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
-	api.POST("/avatars", userHandler.UploadAvatar)
+	api.POST("/avatars", authMiddleware(authService, userService), userHandler.UploadAvatar)
 
 	router.Run()
+}
 
-	// input
-	// handler mapping input to struct
-	// service mapping struct User
-	// repository save struct User to db
-	// db
+func authMiddleware(authService auth.Service, userService user.Service) gin.HandlerFunc {
+
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+
+		if !strings.Contains(authHeader, "Bearer") {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		// Bearer hardcode
+		tokenString := ""
+		arrayToken := strings.Split(authHeader, " ")
+		if len(tokenString) == 2 {
+			tokenString = arrayToken[1]
+		}
+
+		token, err := authService.ValidateToken(tokenString)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		claim, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+
+		userID := int(claim["user_id"].(float64))
+
+		user, err := userService.GetUserByID(userID)
+		if err != nil {
+			response := helper.APIResponse("Unauthorized", http.StatusUnauthorized, "error", nil)
+			c.AbortWithStatusJSON(http.StatusUnauthorized, response)
+			return
+		}
+		c.Set("currentUser", user)
+	}
 }

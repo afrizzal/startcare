b63d5aa132b12a8188a9f2f330043141a19a8101
Afrizzal P. Pratama <apratama8@gmail.com>
2021-09-30 04:54:50 +0700

    add entity & campaign repository

---
+++ campaign/entity.go
@@ -0,0 +1,27 @@
+package campaign
+
+import "time"
+
+type campaign struct {
+	ID               int
+	UserID           int
+	Name             string
+	ShortDescription string
+	Description      string
+	Perks            string
+	BackerCount      int
+	GoalAmount       int
+	CurrentAmount    int
+	Slug             string
+	CreatedAt        time.Time
+	UpdatedAt        time.Time
+}
+
+type CampaignImage struct {
+	ID         int
+	CampaignID int
+	FileName   string
+	IsPrimary  int
+	CreatedAt  time.Time
+	UpdatedAt  time.Time
+}
